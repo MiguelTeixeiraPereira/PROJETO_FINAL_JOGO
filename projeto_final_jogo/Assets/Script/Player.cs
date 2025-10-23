0452386fcec79e4283a4a2d07e59bf616007bfcb
MiguelTeixeiraPereira <miguel.264123@alunosatc.edu.br>
2025-10-23 13:59:30 -0300

    Player

---
+++ projeto_final_jogo/Assets/Script/Player.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //public Vector2 posicaoInicial;
+    //public GameManager gameManager;
+
+    public Animator anim;
+    private Rigidbody2D rigd;
+    public float speed;
+    public float jumpForce = 5f;
+    private bool isground;
+
+  
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigd = GetComponent<Rigidbody2D>();
+        //posicaoInicial = transform.position;
+    }
+
+
+    void Update()
+    {
+        Move();
+        Jump();
+
+    }
+    void Move()
+    {
+        float teclas = Input.GetAxis("Horizontal");
+        rigd.linearVelocity = new Vector2(teclas * speed, rigd.linearVelocity.y);
+
+        if (teclas > 0 && isground == true)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+            anim.SetInteger("transitions", 1);
+        }
+        if (teclas < 0 && isground == true)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+            anim.SetInteger("transitions", 1);
+        }
+        if (teclas == 0 && isground == true)
+        {
+            anim.SetInteger("transitions", 0);
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isground == true)
+        {
+            rigd.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            anim.SetInteger("transitions", 2);
+            isground = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "tagGround")
+        {
+            isground = true;
+            anim.SetInteger("transitions", 0);
+        }
+    }
+
+    public void Reiniciar_posicao()
+    {
+        //transform.position = posicaoInicial;
+    }
+}
